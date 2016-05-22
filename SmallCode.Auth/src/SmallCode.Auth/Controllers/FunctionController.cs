231d3ffc01515e5c2bb5d7f59b1aa061294a5bc3
applenele <1941973283@qq.com>
2016-05-23 01:12:14 +0800

    role，function

--- SmallCode.Auth/src/SmallCode.Auth/Controllers/FunctionController.cs
+++ SmallCode.Auth/src/SmallCode.Auth/Controllers/FunctionController.cs
@@ -3,16 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using SmallCode.Auth.DataModel;
+using SmallCode.Auth.Models;
+using SmallCode.Auth.Extensions;
 
 namespace SmallCode.Auth.Controllers
 {
-    public class FunctionController : Controller
+    public class FunctionController : BaseController
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 分页获取功能
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public IActionResult FunctionList(PageRequestModel model)
+        {
+            IQueryable<Function> query = db.Functions.AsQueryable();
+            if (!string.IsNullOrEmpty(model.key))
+            {
+                query = query.Where(x => x.FunctionName.Contains(model.key));
+            }
+            if (!string.IsNullOrEmpty(model.sortOrder))
+            {
+                bool flag = model.sortOrder.Equals("desc", StringComparison.OrdinalIgnoreCase);
+                query = query.OrderBy(model.sortField, flag);
+            }
+            int index = model.pageIndex * model.pageSize;
+
+            List<Function> functions = query.Skip(index).Take(model.pageSize).ToList();
+            return Json(functions);
+        }
+
+
+        #region 编辑方法
+        /// <summary>
+        /// 编辑方法
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Edit(Guid? id)
+        {
+            Function function = new Models.Function();
+            bool IsInsert = true;
+            if (id.HasValue)
+            {
+                function = db.Functions.First(x => x.Id == id);
+                IsInsert = false;
+            }
+            ViewBag.IsInsert = IsInsert;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Function function)
+        {
+            function.CreatedBy = CurrentUser.Id;
+            function.CreatedDate = DateTime.Now;
+            function.IsDelete = false;
+            db.Functions.Add(function);
+            db.SaveChanges();
+            return View();
+        }
+        #endregion
+
+        public IActionResult Remove(string ids)
+        {
+            if (string.IsNullOrEmpty(ids)) return Content("error");
+            List<Guid> idList = new List<Guid>();
+            ids.Split(',').ToList().ForEach(x => idList.Add(new Guid(x)));
+            List<Function> functions = db.Functions.Where(x => idList.Contains(x.Id)).ToList();
+            db.Functions.RemoveRange(functions);
+            db.SaveChanges();
+            return Content("ok");
         }
     }
 }
--- SmallCode.Auth/src/SmallCode.Auth/Controllers/RoleController.cs
+++ SmallCode.Auth/src/SmallCode.Auth/Controllers/RoleController.cs
@@ -3,17 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using SmallCode.Auth.DataModel;
+using SmallCode.Auth.Models;
+using SmallCode.Auth.Extensions;
 
 namespace SmallCode.Auth.Controllers
 {
-    public class RoleController : Controller
+    public class RoleController : BaseController
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// 分页获取
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public IActionResult RoleList(PageRequestModel model)
+        {
+            IQueryable<Role> query = db.Roles.AsQueryable();
+            if (!string.IsNullOrEmpty(model.key))
+            {
+                query = query.Where(x => x.RoleName.Contains(model.key));
+            }
+            if (!string.IsNullOrEmpty(model.sortOrder))
+            {
+                bool flag = model.sortOrder.Equals("desc", StringComparison.OrdinalIgnoreCase);
+                query = query.OrderBy(model.sortField, flag);
+            }
+            int index = model.pageIndex * model.pageSize;
+
+            List<Role> roles = query.Skip(index).Take(model.pageSize).ToList();
+            return Json(roles);
+        }
+
+
+        #region 角色修改
+        /// <summary>
+        /// 角色修改
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Edit(Guid? id)
+        {
+            Role role = new Models.Role();
+            bool IsInsert = true;
+            if (id.HasValue)
+            {
+                role = db.Roles.First(x => x.Id == id);
+                IsInsert = false;
+            }
+            ViewBag.IsInsert = IsInsert;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Role role)
+        {
+            role.CreatedBy = CurrentUser.Id;
+            role.CreatedDate = DateTime.Now;
+            role.IsDelete = false;
+            db.Roles.Add(role);
+            db.SaveChanges();
+            return View();
+        }
+        #endregion
+
+
+        public IActionResult Remove(string ids)
+        {
+            if (string.IsNullOrEmpty(ids)) return Content("error");
+            List<Guid> idList = new List<Guid>();
+            ids.Split(',').ToList().ForEach(x => idList.Add(new Guid(x)));
+            List<Role> roles = db.Roles.Where(x => idList.Contains(x.Id)).ToList();
+            db.Roles.RemoveRange(roles);
+            db.SaveChanges();
+            return Content("ok");
+        }
+
     }
 }
