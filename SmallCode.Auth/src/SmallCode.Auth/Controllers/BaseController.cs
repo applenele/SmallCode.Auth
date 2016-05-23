6e0be9b9b0dbf9f2d0169d1ad6f36ef3b9af19a1
applenele <1941973283@qq.com>
2016-05-24 01:06:42 +0800

    user privilefes in basecontroller

--- SmallCode.Auth/src/SmallCode.Auth/Controllers/BaseController.cs
+++ SmallCode.Auth/src/SmallCode.Auth/Controllers/BaseController.cs
@@ -20,12 +20,34 @@ namespace SmallCode.Auth.Controllers
 
         public User CurrentUser { set; get; }
 
+        public Dictionary<Guid, List<string>> UserPrivileges = new Dictionary<Guid, List<string>>();
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (HttpContext.User.Identity.IsAuthenticated)
             {
                 CurrentUser = db.Users.Where(x => x.UserName == HttpContext.User.Identity.Name).SingleOrDefault();
                 ViewBag.CurrentUser = CurrentUser;
+
+                if (!UserPrivileges.ContainsKey(CurrentUser.Id))
+                {
+                    var data = from f in db.Functions
+                               join rf in db.RoleFunctions on f.Id equals rf.FunctionId
+                               join r in db.Roles on rf.RoleId equals r.Id
+                               join ur in db.UserRoles on r.Id equals ur.RoleId
+                               join u in db.Users on ur.UserId equals u.Id
+                               where u.Id == CurrentUser.Id
+                               select new
+                               {
+                                   f.Url
+                               };
+                    List<string> urls = new List<string>();
+                    foreach (var item in data)
+                    {
+                        urls.Add(item.Url);
+                    }
+                    UserPrivileges.Add(CurrentUser.Id, urls);
+                }
             }
             ViewBag.CurrentUser = CurrentUser;
             base.OnActionExecuting(context);
